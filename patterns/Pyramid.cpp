e4ae7845378321bb66e7fd89f02ca8d7f48e2b5a
srikarnalla <srikarnalla004@gmail.com>
2026-03-29 16:33:32 +0530

    Add pyramid pattern printing function
    Implement a function to print a pyramid pattern using nested loops.

---
+++ patterns/Pyramid.cpp
@@ -0,0 +1,30 @@
+// Problem: Print pyramid Pattern (4x4)
+// Pattern Type: Basic Nested Loop
+
+// Idea:
+// Use two loops:
+// Outer loop → rows
+// Inner loop → columns 
+// Print "*" for each column
+
+// Time Complexity: O(n^2)
+// Space Complexity: O(1)
+
+
+
+#include <bits/stdc++.h>
+ 
+using namespace std;
+
+void print(int n){
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j <= i; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+
+int main(){
+    print(4);
+}
