aa7a62af8546d4f1a622a8bb88d4fcd12bf65a3e
srikarnalla <srikarnalla004@gmail.com>
2026-03-31 20:21:17 +0530

    Create increasing_letter.cpp

---
+++ patterns/increasing_letter.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void print(int n){
+    char start = 'A';
+    for (int i = 1; i <= n; i++){
+        for (int j = 1; j <= i; j++){
+            cout<<start;
+            start++;
+        }
+        start='A';
+        cout<<endl;
+    }
+}
+
+int main(){
+    print(5);
+}
