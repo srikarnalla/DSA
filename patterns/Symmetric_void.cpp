7c11644507a61a2096a064f34ac252e8999b21cd
srikarnalla <srikarnalla004@gmail.com>
2026-04-01 23:48:46 +0530

    Create Symmetric_void.cpp

---
+++ patterns/Symmetric_void.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void print(int n){
+for (int i = 0; i < n; i++){
+    for (int j = n-i; j > 0; j--){
+        cout<<'*';
+    }
+    for (int k=0;k<2*i;k++){
+        cout<<" ";
+    }
+    for (int j =n-i;j>0;j--){
+        cout<<'*';
+    }
+    cout << endl;
+    }
+for (int i = 0; i < n; i++){
+    for (int j = 0; j <= i; j++){
+        cout<<'*';
+    }
+    for (int k=2*n;k>2*i+2;k--){
+        cout<<" ";
+    }
+    for (int j =0;j<=i;j++){
+        cout<<'*';
+    }
+    cout << endl;
+    }
+}
+
+int main(){
+print(5);
+return 0;
+}
