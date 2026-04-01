bbc82caccca139f950c3db194277d230ba4ee8d7
srikarnalla <srikarnalla004@gmail.com>
2026-04-02 00:05:00 +0530

    Create hollow_rectangle.cpp

---
+++ patterns/hollow_rectangle.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void print(int n){
+for (int i = 0; i < n; i++){
+        for (int j=0;j<n;j++){
+                if (i==0 || i==n-1 || j==0||j==n-1){
+                cout<<"*";
+            }
+            else{
+                cout<<' ';
+            }
+        }
+        cout<<endl;
+    }
+}
+
+
+int main(){
+print(3);
+return 0;
+}
