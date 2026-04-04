8bc156b3e9e8af7fd9a91d5a0b0ae40121992326
srikarnalla <srikarnalla004@gmail.com>
2026-04-04 21:55:17 +0530

    Create mutli_unordered_set.cpp

---
+++ STL/Containers/mutli_unordered_set.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    mutliset <int> ms;
+    ms.insert(9);
+    ms.erase(9);
+    int cnt = ms.count(9);
+    auto it =ms.find(1);
+    ms.erase(ms.find(1));
+}
