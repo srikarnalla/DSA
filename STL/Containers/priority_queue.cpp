9eccfb965362253009dc2bad9eaed39e0fa9a8ab
srikarnalla <srikarnalla004@gmail.com>
2026-04-04 21:54:02 +0530

    Add example of using priority_queue in C++
    Implement a simple priority queue example in C++.

---
+++ STL/Containers/priority_queue.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    priority_queue<int> pq;
+    pq.push(3);
+    pq.push(2);
+    pq.push(34);
+
+    while(!pq.empty()){
+        cout<<pq.top()<<" ";
+        pq.pop();
+    }
+}
