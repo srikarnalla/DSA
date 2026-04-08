68ead9ba15bff82b533769432b4f76180595b589
srikarnalla <srikarnalla004@gmail.com>
2026-04-08 18:09:07 +0530

    Create add_print.cpp

---
+++ Math/recursions/add_print.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void name(int i, int n){
+    //Base case
+    if (i > n){
+        return;
+    }
+    cout<<("name")<<endl;
+    name(i+1,n);
+}
+void name1(int i, int n){
+    if (i>n){
+        return;
+    }
+    cout<<i<<" ";
+    name1(i+1,n);
+}
+void name2(int i, int n){
+    if (n < i){
+        return;
+    }
+    cout<<n<<" ";
+    name2(i,n-1);
+}
+void no_backtracking(int i, int n){
+    if (i < 1){
+        return;
+    }
+    no_backtracking(i-1,n);
+    cout<<i<<" ";
+}
+void no_backtracking1(int i){
+    if (i < 1){
+        return;
+    }
+    no_backtracking1(i-1);
+    cout<<i<<" ";
+}
+void sum1(int n, int sum2){
+    if (n<1){
+        cout<<sum2<<endl;
+        return; 
+    }
+    sum1(n-1,sum2+n);
+}
+int main(){
+    int n;
+    cin>>n;
+    /*
+    name(1,n);
+    name1(1,n);
+    cout<<endl;
+    name2(1,n);
+    cout<<endl;
+    no_backtracking(3,3);
+    no_backtracking1(3);*/
+    sum1(10,0);
+}
