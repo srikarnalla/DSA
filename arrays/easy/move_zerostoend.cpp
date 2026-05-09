92969fabb8e276bd77c78dc85ce396c4168233d8
srikarnalla <srikarnalla004@gmail.com>
2026-05-09 23:52:41 +0530

    Implement optimal zeroes moving method
    Added an optimal method to move zeroes to the end of the array.

--- arrays/easy/move_zerostoend.cpp
+++ arrays/easy/move_zerostoend.cpp
@@ -1,4 +1,5 @@
 class Solution {
+//bruteforce
 public:
     void moveZeroes(vector<int>& nums) {
     vector <int> temp;
@@ -8,6 +9,7 @@ public:
             temp.push_back(nums[i]);
         }
     }
+        
     int m = temp.size();
     for (int i = 0; i < n; i++){
         if (i<m){
@@ -19,3 +21,22 @@ public:
     }
     }
 };
+
+//optimal method
+void moveZeroes(vector<int>& nums) {
+    int n = nums.size();
+    int j = -1;
+    for (int i = 0; i < n; i++){
+        if (nums[i] == 0){
+            j = i;
+            break;
+        }
+    }
+    if (j == -1) return;
+    for (int i = j+1; i < n; i++){
+        if (nums[i]!=0){
+            swap(nums[i],nums[j]);
+            j++;
+        }
+    }
+    }
