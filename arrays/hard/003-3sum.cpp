1e3ce252bf635edf376d56261052cf39409ba4d0
srikarnalla <srikarnalla004@gmail.com>
2026-06-19 23:07:16 +0530

    Update 003-3sum.cpp

--- arrays/hard/003-3sum.cpp
+++ arrays/hard/003-3sum.cpp
@@ -19,3 +19,49 @@ vector<vector<int>> brute(vector<int> nums){
     vector<vector<int>> ans(st.begin(),st.end());
     return ans;
 }
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& arr) {
+        int n = arr.size();
+        sort(arr.begin(), arr.end());
+
+        vector<vector<int>> ans;
+
+        for (int i = 0; i < n; i++) {
+
+            if (i > 0 && arr[i] == arr[i - 1]) continue;
+
+            if (arr[i] > 0) break;
+
+            int left = i + 1;
+            int right = n - 1;
+
+            while (left < right) {
+
+                long long sum = (long long)arr[i] + arr[left] + arr[right];
+
+                if (sum == 0) {
+
+                    ans.push_back({arr[i], arr[left], arr[right]});
+
+                    left++;
+                    right--;
+
+                    while (left < right && arr[left] == arr[left - 1])
+                        left++;
+
+                    while (left < right && arr[right] == arr[right + 1])
+                        right--;
+                }
+                else if (sum < 0) {
+                left++;
+                }
+                else {
+                    right--;
+                }
+            }
+        }
+    return ans;
+    }
+};
