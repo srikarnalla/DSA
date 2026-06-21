1132e03a31d946f671bf02a975d369ade5c0ada1
srikarnalla <srikarnalla004@gmail.com>
2026-06-21 21:22:33 +0530

    Update 003-3sum.cpp

--- arrays/hard/003-3sum.cpp
+++ arrays/hard/003-3sum.cpp
@@ -20,48 +20,44 @@ vector<vector<int>> brute(vector<int> nums){
     return ans;
 }
 
-class Solution {
-public:
-    vector<vector<int>> threeSum(vector<int>& arr) {
-        int n = arr.size();
-        sort(arr.begin(), arr.end());
-
-        vector<vector<int>> ans;
-
-        for (int i = 0; i < n; i++) {
-
-            if (i > 0 && arr[i] == arr[i - 1]) continue;
-
-            if (arr[i] > 0) break;
-
-            int left = i + 1;
-            int right = n - 1;
-
-            while (left < right) {
-
-                long long sum = (long long)arr[i] + arr[left] + arr[right];
-
-                if (sum == 0) {
-
-                    ans.push_back({arr[i], arr[left], arr[right]});
-
-                    left++;
-                    right--;
-
-                    while (left < right && arr[left] == arr[left - 1])
-                        left++;
-
-                    while (left < right && arr[right] == arr[right + 1])
-                        right--;
-                }
-                else if (sum < 0) {
-                left++;
-                }
-                else {
-                    right--;
-                }
+vector<vector<int>> better(vector<int> nums){
+    int n = nums.size();
+    set<vector<int>> st;
+    for (int i = 0; i < n; i++){
+        set<int> hashst;
+        for (int j = i+1; j < n; j++){   
+            if (hashst.find(-1*(nums[i]+nums[j])) != hashst.end()){
+                vector<int> temp = {nums[i],nums[j],-1*(nums[i] + nums[j])};
+                sort(temp.begin(),temp.end());
+                st.insert(temp);
             }
+            hashst.insert[nums[j]];
         }
-    return ans;
     }
-};
+    vector<vector<int>> ans(st.begin(),st.end());
+    return ans;
+}
+
+vector<vector<int>> optimal(vector<int> nums){
+    int n =nums.size();
+    sort(nums.begin(), nums.end());
+    for (int i = 0; i < n; i++){
+        if ( i> o && nums[i] == nums[i-1]) continue;
+        int j = i+1, k = n-1;
+        while (j < k){
+            int s = nums[i] + nums[j] + nums[k];
+            if (s == 0){
+                ans.push_back(nums[i] , nums[j] , nums[k]);
+                j++;
+                k--;
+                while(j <= k && nums[j-1] == nums[j]) j++;
+
+                while(j<= k && nums[k+1] == nums[k]) k--;
+            } 
+            else if (s > 0 ) j++;
+            else k--;
+        }
+    }
+    return ans;
+}
+
